908b7446c9104c2fd2f7c9c2e892117096bc03a3
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-05-10 20:55:24 +0530

    - Update the logic for star selection and hover behavior. - Fix the typo.

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const StarRating = ({ noOfStars = 5 }) => {
     const [rating, setRating] = useState(0);
-    const [hoverIndex, setHoverIndex] = useState(0);
+    const [hoverIndex, setHoverIndex] = useState(null);
     return (
         <div className="wrapper p-5 bg-amber-400">
             <h2 className="text-4xl text-center my-5 text-white">Star Rating Demo</h2>
@@ -10,11 +10,11 @@ const StarRating = ({ noOfStars = 5 }) => {
                 {...Array.from({ length: noOfStars }, (_, index) => (
                     <box-icon
                         key={index}
-                        onMouseOver={() => setHoverIndex(index)}
-                        omMouseLeave={() => setHoverIndex(null)}
-                        onClick={() => setRating(index)}
+                        onMouseOver={() => setHoverIndex(index + 1)}
+                        onMouseLeave={() => setHoverIndex(null)}
+                        onClick={() => setRating(index + 1)}
                         name="star"
-                        type={hoverIndex >= index ? 'solid' : 'regular'}
+                        type={hoverIndex > index || rating > index ? 'solid' : 'regular'}
                         className="cursor-pointer"
                     ></box-icon>
                 ))}
