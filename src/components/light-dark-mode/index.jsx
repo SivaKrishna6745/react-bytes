dd88c21ae6d4659c2be24ab8109dd5a57cfc98f2
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-05-22 13:30:22 +0530

    - Initialize LightDarkMode component. - Integrate LightDarkMode component into App.jsx.

---
+++ src/components/light-dark-mode/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LightDarkMode = () => {
+    return (
+        <div className="wrapper p-5 bg-white flex flex-col justify-center items-center">
+            <h2 className="text-4xl text-black">Light Dark Mode Demo</h2>
+        </div>
+    );
+};
+
+export default LightDarkMode;
