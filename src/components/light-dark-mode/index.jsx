02a043996c7ecd3f9d2848a9e3d3ce3daac142e4
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-05-23 12:19:04 +0530

    - Implement light/dark theme toggle logic. - Add theme persistence using localStorage.

--- src/components/light-dark-mode/index.jsx
+++ src/components/light-dark-mode/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LightDarkMode = () => {
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+    };
+
     return (
-        <div className="wrapper p-5 bg-white flex flex-col justify-center items-center">
-            <h2 className="text-4xl text-black">Light Dark Mode Demo</h2>
+        <div
+            className={`wrapper p-5 ${
+                theme === 'light' ? 'bg-white' : 'bg-black'
+            } flex flex-col gap-4 justify-center items-center`}
+        >
+            <h2 className={`text-4xl ${theme === 'light' ? 'text-black' : 'text-white'}`}>Light Dark Mode Demo</h2>
+            <p className={`${theme === 'light' ? 'text-black' : 'text-white'}`}>Hello World!</p>
+            <button onClick={toggleTheme} className="cursor-pointer">
+                <box-icon
+                    name={theme === 'light' ? 'sun' : 'moon'}
+                    color={`${theme === 'light' ? 'black' : 'white'}`}
+                ></box-icon>
+            </button>
         </div>
     );
 };
