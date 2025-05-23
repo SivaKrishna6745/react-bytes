8065fecf39094c08a515c9e83f4818431afbc7d0
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-05-23 12:40:41 +0530

    - Resolve the key error in the TreeView component.

--- src/components/tree-view/menu-list.jsx
+++ src/components/tree-view/menu-list.jsx
@@ -4,7 +4,7 @@ import MenuItem from './menu-item';
 const MenuList = ({ list = [] }) => {
     return (
         <ul className="container list-none style-[list-style:unset] pl-5">
-            {list && list.length ? list.map((item) => <MenuItem item={item} />) : ''}
+            {list && list.length ? list.map((item) => <MenuItem key={item.label} item={item} />) : ''}
         </ul>
     );
 };
