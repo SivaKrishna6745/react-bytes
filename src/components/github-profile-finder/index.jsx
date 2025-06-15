30873c4d911267cbc9456cc9dd2446bd6b5335b6
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-06-15 11:41:33 +0530

    - Improve form handling for better user experience - Add state variable to track  search state - Improve fetch logic - Update UI styling for a cleaner look

--- src/components/github-profile-finder/index.jsx
+++ src/components/github-profile-finder/index.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import User from './user';
 
 const GithubProfileFinder = () => {
-    const [userName, setUserName] = useState('sivakrishna6745');
+    const [userName, setUserName] = useState('');
     const [userData, setUserData] = useState(null);
+    const [hasSearched, setHasSearched] = useState(false);
     const [loading, setLoading] = useState(false);
     const [err, setErr] = useState('');
 
@@ -11,9 +13,10 @@ const GithubProfileFinder = () => {
         setUserName(name);
     };
     useEffect(() => {
-        fetchGithubUserData();
+        if (hasSearched && userName) fetchGithubUserData();
     }, []);
     const fetchGithubUserData = async () => {
+        setLoading(true);
         try {
             const res = await fetch(`https://api.github.com/users/${userName}`);
             const data = await res.json();
@@ -26,29 +29,45 @@ const GithubProfileFinder = () => {
             setLoading(false);
         }
     };
-    const handleSubmit = () => {
-        console.log('userName', userName);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setHasSearched(true);
+        fetchGithubUserData();
+        setUserName('');
     };
 
     if (err) return <div className="text-2xl text-white">Data fetch failed!! Try again later...</div>;
     if (loading) return <div className="text-2xl text-white">Data fetching!! Please wait...</div>;
 
     return (
-        <div className="wrapper p-5 bg-orange-300 flex flex-col items-center justify-center">
+        <div
+            className="wrapper p-5 bg-gradient-to-r from-blue-500 to-purple-500 h-screen flex flex-col gap-8 items-center justify-center"
+            style={{ fontFamily: 'Poppins, sans-serif' }}
+        >
             <h2 className="text-4xl text-white">Github Profile Finder Demo</h2>
-            <div className="mt-10 flex gap-4">
+            <form className="flex gap-4">
                 <input
                     type="text"
                     name="userName"
-                    className="border-b-green-900 border-b-2 outline-none px-1 text-xl"
+                    className="border-b-green-900 border-b-2 outline-none px-1 text-xl focus:bg-transparent"
                     placeholder="Enter Github UserName..."
                     value={userName}
                     onChange={handleUserName}
                 />
-                <button onClick={handleSubmit} className="bg-blue-500 px-6 py-3 rounded-xl text-xl cursor-pointer">
+                <button
+                    type="submit"
+                    onClick={handleSubmit}
+                    className="bg-blue-500 px-6 py-3 outline-none rounded-xl text-xl cursor-pointer transition-all duration-300 ease-in-out hover:bg-blue-600 active:scale-95"
+                >
                     Search
                 </button>
-            </div>
+            </form>
+            {hasSearched &&
+                (userData?.login ? (
+                    <User user={userData} />
+                ) : (
+                    <div className="text-2xl text-white">User Not Found!!</div>
+                ))}
         </div>
     );
 };
