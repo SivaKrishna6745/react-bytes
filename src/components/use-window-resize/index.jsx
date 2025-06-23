f96606dfd175f652ab3fcb3d01fde7df935053af
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-06-23 12:31:11 +0530

    - Add logic for useWindowResize custom hook - Update the test component with revised logic - Styling enhancements

--- src/components/use-window-resize/index.jsx
+++ src/components/use-window-resize/index.jsx
@@ -1,3 +1,24 @@
-const useWindowResize = () => {};
+import { useLayoutEffect, useState } from 'react';
+
+const useWindowResize = () => {
+    const [windowSize, setWindowSize] = useState({
+        width: 0,
+        height: 0,
+    });
+    useLayoutEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return { windowSize };
+};
 
 export default useWindowResize;
--- src/components/use-window-resize/test.jsx
+++ src/components/use-window-resize/test.jsx
@@ -1,10 +1,15 @@
 import useWindowResize from '.';
 
 const UseWindowResizeTest = () => {
-    useWindowResize();
+    const { windowSize } = useWindowResize();
+    const { width, height } = windowSize;
     return (
         <div className="wrapper p-5 bg-red-400 flex flex-col items-center justify-center">
-            <h2 className="text-4xl text-white">useWindowResize Custom Hook Demo</h2>
+            <h2 className="text-4xl text-white mb-8">useWindowResize Custom Hook Demo</h2>
+            <div className="bg-white/60 px-8 py-4 rounded-xl text-2xl flex flex-col gap-4">
+                <p>Width is {width}</p>
+                <p>Height is {height}</p>
+            </div>
         </div>
     );
 };
